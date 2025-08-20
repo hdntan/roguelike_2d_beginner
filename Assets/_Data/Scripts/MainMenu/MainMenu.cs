ad21fd938ff85ee9c58a3836560507d109570793
hdntan <hdntan0903@gmail.com>
2025-08-20 22:47:25 +0700

    connect world settings SO in Menu scene

--- Assets/_Data/Scripts/MainMenu/MainMenu.cs
+++ Assets/_Data/Scripts/MainMenu/MainMenu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
@@ -8,26 +9,38 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private UIDocument uIDocument;
 
-    // public WorldSettings[] WorldSettings;
+    public WorldSO[] worldSettings;
     public Tilemap backgroundTilemap;
 
-    public List<Tile> groundTiles;
+
+
+    [SerializeField] private DropdownField worldSettingsThemeDropdown;
+    [SerializeField] private WorldSO selectedSettings;
 
 
     private void Awake()
     {
         this.uIDocument = GetComponent<UIDocument>();
-       this.backgroundTilemap = GameObject.Find("Grid").GetComponentInChildren<Tilemap>();
+        this.backgroundTilemap = GameObject.Find("Grid").GetComponentInChildren<Tilemap>();
         //this.continueButton = this.uIDocument.rootVisualElement.Q<Button>("Continue");
         var newButton = this.uIDocument.rootVisualElement.Q<Button>("StartGame");
         var quitButton = this.uIDocument.rootVisualElement.Q<Button>("QuitGame");
-        var themeDropdown = this.uIDocument.rootVisualElement.Q<DropdownField>("ThemeDropdown");
-        Debug.Log("MainMenu Awake called" + themeDropdown + " " + newButton + " " + quitButton);
+        this.worldSettingsThemeDropdown = this.uIDocument.rootVisualElement.Q<DropdownField>("ThemeDropdown");
+
         newButton.clicked += StartNewGame;
         quitButton.clicked += Application.Quit;
-        themeDropdown.choices = new List<string> { "Snow", "Desert", "Forest", "Dungeon" };
-        themeDropdown.value = "Snow";
-        this.FillBackgroundFromTheme();
+
+        this.selectedSettings = this.worldSettings[0];
+        var worldNames = this.worldSettings.Select(settings => settings.worldName).ToList();
+        this.worldSettingsThemeDropdown.choices = worldNames;
+
+
+        this.worldSettingsThemeDropdown.RegisterValueChangedCallback(evt =>
+        {
+            this.selectedSettings = this.worldSettings[this.worldSettingsThemeDropdown.index];
+            FillBackgroundFromTheme();
+        });
+        this.worldSettingsThemeDropdown.index = 0;
     }
 
     void StartNewGame()
@@ -42,13 +55,8 @@ public class MainMenu : MonoBehaviour
         {
             for (int x = -50; x < 50; ++x)
             {
-                this.backgroundTilemap.SetTile(new Vector3Int(x, y, 0), this.RandomTileGround());
+                this.backgroundTilemap.SetTile(new Vector3Int(x, y, 0), this.selectedSettings.theme.GetRandomGround());
             }
         }
     }
-    public Tile RandomTileGround()
-    {
-            return groundTiles[Random.Range(0, groundTiles.Count)];
-        }
-
 }
